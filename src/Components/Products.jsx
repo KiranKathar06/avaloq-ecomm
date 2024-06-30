7f6998b603debf3d667e7e214024be8775dbaf1c
Kiran Kathar <kkathar4@gmail.com>
2024-06-30 22:20:57 +0530

    styled: searchbar

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -7,9 +7,9 @@ import Searchbar from './Searchbar'; // Adjust the path according to your actual
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
- 
+
   const getProducts = async () => {
-    
+
     try {
       const apiData = await axios.get('https://dummyjson.com/products');
       const fetchedProducts = apiData.data.products;
@@ -25,26 +25,26 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="container mt-5">
-      {/* SearchBar Component */}
-    
+    <>
       <Searchbar products={products} setFilteredProducts={setFilteredProducts} />
-      
-      <div className="row">
-        {filteredProducts.map((data) => (
-          <div key={data.id} className="col-md-4 mb-4">
-            <Card className="h-100">
-              <Card.Img variant="top" src={data.images[0]} alt={data.title} />
-              <Card.Body>
-                <Card.Title>{data.title}</Card.Title>
-                <Card.Text>${data.price}</Card.Text>
-                <button className="btn btn-primary" >Add to cart</button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+
+      <div className="container mt-5">
+        <div className="row">
+          {filteredProducts.map((data) => (
+            <div key={data.id} className="col-md-4 mb-4">
+              <Card className="h-100">
+                <Card.Img variant="top" src={data.images[0]} alt={data.title} />
+                <Card.Body>
+                  <Card.Title>{data.title}</Card.Title>
+                  <Card.Text>${data.price}</Card.Text>
+                  <button className="btn btn-primary" >Add to cart</button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 //export const products = products;
